0ace07f58f938e10c695981feb62f8c0953e8d6d
Zarar-Anwar <zararanwar1234321@gmail.com>
2023-10-07 19:27:26 +0500

    REMOVE : EXTRA

--- check.py
+++
@@ -1,8 +0,0 @@
-import re
-
-original_string = r"D:\PROJECTS\glucoma_detect\glucoma_detect\website"
-
-# Use regular expressions to remove the last 8 words
-result_string = re.sub(r'(\\[^\\]+){8}$', '', original_string)
-
-print(result_string)
